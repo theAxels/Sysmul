02076ce9103e4c8859fb9bc26b48e1fa2699c1a5
theAxels <angkianto.laurence@binus.ac.id>
2024-03-05 08:28:00 +0700

    last

--- js/play.js
+++ js/play.js
@@ -1,9 +1,64 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     var iframe = document.querySelector('#youtubeVideo');
     var src = iframe.getAttribute('src');
     src += '&autoplay=1';
     iframe.setAttribute('src', src);
 });
+
+var carousels = document.querySelectorAll('.carousel');
+
+carousels.forEach(function(carousel) {
+    var carouselInterval = setInterval(function() {
+        var activeItem = carousel.querySelector('.carousel-item.active');
+        var nextItem = activeItem.nextElementSibling;
+        var activeIndicator = carousel.querySelector('.carousel-indicators .active');
+        var nextIndicator;
+    
+        if (!nextItem) {
+            nextItem = carousel.querySelector('.carousel-item:first-child');
+            nextIndicator = carousel.querySelector('.carousel-indicators li:first-child');
+        } else {
+            nextIndicator = activeIndicator.nextElementSibling;
+        }
+    
+        nextItem.classList.add('active');
+        activeItem.classList.remove('active');
+    
+        activeIndicator.classList.remove('active');
+        nextIndicator.classList.add('active');
+    }, 5000);
+
+    carousel.querySelectorAll('.carousel-indicators li').forEach(function(indicator, index) {
+        indicator.addEventListener('click', function() {
+            var targetSlideIndex = parseInt(indicator.getAttribute('data-bs-slide-to'));
+            goToSlide(carousel, targetSlideIndex);
+        });
+    });
+});
+
+function goToSlide(carousel, targetSlideIndex) {
+    var carouselInner = carousel.querySelector('.carousel-inner');
+    var slides = carouselInner.querySelectorAll('.carousel-item');
+    slides.forEach(function(slide) {
+        slide.classList.remove('active');
+    });
+    slides[targetSlideIndex].classList.add('active');
+    var indicators = carousel.querySelectorAll('.carousel-indicators li');
+    indicators.forEach(function(indicator) {
+        indicator.classList.remove('active');
+    });
+    indicators[targetSlideIndex].classList.add('active');
+}
+
 
 function toggleSynopsis() {
     var synopsisContent = document.querySelector('.synopsis-content');
