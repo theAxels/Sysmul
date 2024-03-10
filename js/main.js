1da7282935af9555ab725dbad4b86bf49e149482
theAxels <127481633+theAxels@users.noreply.github.com>
2024-03-11 00:19:51 +0700

    update gtt js

--- js/main.js
+++ js/main.js
@@ -9,16 +9,5 @@ function scrollFunction() {
 }
 
 function goToTop() {
-    const duration = 500;
-    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollTop > 0) {
-      const scrollStep = -scrollTop / (duration / 15);
-      const scrollInterval = setInterval(function() {
-        if (document.documentElement.scrollTop || document.body.scrollTop) {
-          window.scrollBy(0, scrollStep);
-        } else {
-          clearInterval(scrollInterval);
-        }
-      }, 15);
-    }
-  }
+  window.scrollTo(0, 0);
+}
